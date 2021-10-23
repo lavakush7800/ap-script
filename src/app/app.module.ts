dc09380dc6c822a2e5d1a58b1011ff3d0b8e7815
lacakush7800 <lavakushgiri@gmail.com>
2021-10-23 22:02:42 +0530

    modify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SideNavbarComponent } from './layout/side-navbar/side-navbar.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideNavbarComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
