832f847d0847f873d8e873f6b90f59bc471729e1
lacakush7800 <lavakushgiri@gmail.com>
2021-11-05 23:28:44 +0530

    remove code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './layout/layout.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { SideNavbarComponent } from './layout/side-navbar/side-navbar.component';
-import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent,
-    HeaderComponent,
-    FooterComponent,
-    SideNavbarComponent,
-    DemoComponent
   ],
   imports: [
     BrowserModule,
