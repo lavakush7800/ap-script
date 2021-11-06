4d7c099777154cecc808f102abc5d7b2f2933c66
lacakush7800 <lavakushgiri@gmail.com>
2021-11-06 15:12:02 +0530

    show header footer sidebar

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -4,6 +4,7 @@ import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -16,6 +17,7 @@ import { PostsComponent } from 'src/app/modules/posts/posts.component';
   imports: [
     CommonModule,
     RouterModule,
+    SharedModule,
   ]
 })
 export class DefaultModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
