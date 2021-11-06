38f4902d3d3eca5c8196e75e6d0ccb748813377b
lacakush7800 <lavakushgiri@gmail.com>
2021-11-06 15:01:33 +0530

    modifycode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { DefaultModule } from './layouts/default/default.module';
-import { PostsComponent } from './modules/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent,
   ],
   imports: [
     BrowserModule,
