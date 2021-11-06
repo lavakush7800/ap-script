3fbaf7a3dbacb2a2b8a72ad7c679582f560c2e5f
lacakush7800 <lavakushgiri@gmail.com>
2021-11-06 14:58:15 +0530

    genrate moduole shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { DefaultModule } from './layouts/default/default.module';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent,
-
   ],
   imports: [
     BrowserModule,
