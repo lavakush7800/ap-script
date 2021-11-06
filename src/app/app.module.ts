9b7bac1dbccd721245b08d5ef5c0a8923d201f01
lacakush7800 <lavakushgiri@gmail.com>
2021-11-06 14:45:01 +0530

    henrate component header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { DefaultModule } from './layouts/default/default.module';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
 
   ],
   imports: [
