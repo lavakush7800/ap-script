903a6e19cc6819f68d727d49d9113ce95a976967
lacakush7800 <lavakushgiri@gmail.com>
2021-11-06 14:28:10 +0530

    modify code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { DefaultModule } from './layouts/default/default.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    DefaultModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+  ],
   imports: [
     CommonModule
   ]
