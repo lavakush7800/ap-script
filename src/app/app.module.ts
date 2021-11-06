428c43cc5c1c28ba5e7816e506223d8902622d9e
lacakush7800 <lavakushgiri@gmail.com>
2021-11-06 14:23:35 +0530

    modify code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import { DefaultComponent } from './layouts/default/default.component';
-import { DashboardComponent } from './modules/dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefaultComponent,
-    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
