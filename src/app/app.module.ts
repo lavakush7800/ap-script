9899d1c3a2555884eda5b041ed09b28f70ddc75a
lacakush7800 <lavakushgiri@gmail.com>
2021-11-08 21:23:47 +0530

    modify code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
