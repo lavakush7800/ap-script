9920cfda3f89df2515475f70f1bcc93037065c53
lacakush7800 <lavakushgiri@gmail.com>
2021-11-22 08:08:52 +0530

    add layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent,
   ],
   imports: [
     BrowserModule,
